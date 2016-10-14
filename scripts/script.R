67d831f14d5e095b95cfee7654b9098e96abc8e1
Benjamin Chan <chanb@ohsu.edu>
2016-10-14 12:01:45 -0700

    Create script.R

---
+++ scripts/script.R
@@ -0,0 +1,25 @@
+Sys.time0 <- Sys.time()
+
+sink("script.log")
+files <- c("preamble.Rmd",
+           "readData.Rmd",
+           "exploreTrain.Rmd",
+           "modelTrain.Rmd")
+f <- file("master.Rmd", open = "w")
+for (i in 1:length(files)) {
+    x <- readLines(files[i])
+    writeLines(x, f)
+    if (i < length(files)) {writeLines("\n---\n", f)}
+}
+close(f)
+library(knitr)
+library(rmarkdown)
+knit("master.Rmd", output = "index.md")
+file.remove("master.Rmd")
+sink()
+
+sink("session.log")
+list(completionDateTime = Sys.time(),
+     executionTime = Sys.time() - Sys.time0,
+     sessionInfo = sessionInfo())
+sink()
