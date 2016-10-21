cb7c2e3350663d4abfd6c2d0c5ff455f98be07ec
Benjamin Chan <chanb@ohsu.edu>
2016-10-21 09:20:04 -0700

    Fix summaryMAE

--- lib/summaryMAE.R
+++ lib/summaryMAE.R
@@ -1,9 +1,8 @@
 summaryMAE <- function (data, lev = NULL, model = NULL) {
-    # Summary function for the caret package.skeleton
+    # Summary function for the caret package
     # Uses the mean absolute error
     # See http://stackoverflow.com/a/22508678/1427069
-    # See mae.R
-    out <- mae(data$pred, data$obs)  
+    out <- mean(abs(data$obs - data$pred), na.rm = TRUE)
     names(out) <- "MAE"
     out
 }
